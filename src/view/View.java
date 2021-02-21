59b4655ce35e8735374812329939eb90e9329d41
Christoffer Grännby <cribbster1987@hotmail.com>
2021-02-21 11:56:42 +0100

    comments added

--- src/view/View.java
+++ src/view/View.java
@@ -8,4 +8,20 @@ package view;
  * Package: view
  */
 public class View {
+
+    // låta användaren lägga in produkter. ska ej kunna se databasens IDn
+
+    // skriv in användarnan & lösenord
+
+    // skriv ut alla produkter i lager och välj produkt
+
+    // stored procedures, produkten läggs till i beställningen
+
+    // återkoppling om fel uppstod eller om det gått bra att lägga till varan
+
+    // möjlighet att skriva ut alla produkter som lagts i varukorgen
+
+    // betyg och kommentarer ska kunna GES på produkter (VG)
+
+    // betyg och kommentarer ska kunna SES på produkter (VG)
 }

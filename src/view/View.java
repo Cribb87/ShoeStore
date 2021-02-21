cdb9a296d0f9aa94e97f4ec08330a88304453205
Christoffer Grännby <cribbster1987@hotmail.com>
2021-02-21 21:23:56 +0100

    Lade till stockBalance

--- src/view/View.java
+++ src/view/View.java
@@ -1,7 +1,10 @@
 package view;
 
 import model.Repository;
+import model.Reviews;
 
+import java.sql.*;
+import java.util.Properties;
 import java.util.Scanner;
 
 /**
@@ -14,24 +17,66 @@ import java.util.Scanner;
 public class View {
 
     // låta användaren lägga in produkter. ska ej kunna se databasens IDn
-    public View(){
+    public View() throws SQLException {
         Repository repo = new Repository();
         String choise;
+        int alternative;
         Scanner scan = new Scanner(System.in);
 
-        while(true) {
-            System.out.print("What product do you want to buy? (Type Q for exit)");
+        System.out.println("Välkommen till SkoShoppen");
+        System.out.println("Välj ett alternativ");
+        System.out.println("1. Gör en beställning");
+        System.out.println("2. Lägg till en vara i en befintlig beställning");
+        System.out.println("3. Kolla betyg på en vara");
+        System.out.println("4. Kolla vilka produkter som finns i lager");
+
+        alternative = scan.nextInt();
+
+        switch (alternative){
+            case 1:
+                repo.getOrder();
+                break;
+            case 2:
+                repo.addToCart();
+                break;
+            case 3:
+
+                break;
+            case 4:
+                stockBalance();
+                break;
+        }
+
+      /*  while(true) {
+            System.out.print("Vilken sko vill du lägga till? (Skriv Q för att avsluta)");
             choise = scan.nextLine().trim();
             if (choise.equalsIgnoreCase("Q")){
                 System.exit(0);
             }
                 System.out.println(repo.getAllShoes());
-        }
+        }*/
     }
 
-    // skriv in användarnan & lösenord
+    // skriv in användarnamn & lösenord
 
     // skriv ut alla produkter i lager och välj produkt
+    public int stockBalance() throws SQLException {
+        Properties properties = new Properties();
+        try(Connection connection = DriverManager.getConnection(properties.getProperty("connectionString"),
+                properties.getProperty("name"),
+                properties.getProperty("password"));
+            PreparedStatement statement = connection.prepareStatement("select * from stockbalance" +
+                    "join shoe on shoe.id = stockbalance.shoeID");
+            ResultSet rs = statement.executeQuery()){
+                while(rs.next()){
+                    int quantity = rs.getInt(stockBalance());
+                }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return stockBalance();
+    }
 
     // stored procedures, produkten läggs till i beställningen
 

7814f7d737ae79945c6c5f7df92ba98f340898f0
Christoffer Grännby <cribbster1987@hotmail.com>
2021-02-21 12:19:05 +0100

    Small adds

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,10 @@
 package view;
 
+import model.Repository;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 /**
  * Created by Emil Johansson
  * Date: 2021-02-20
@@ -10,6 +15,14 @@ package view;
 public class View {
 
     // låta användaren lägga in produkter. ska ej kunna se databasens IDn
+    public View(){
+        Repository repo = new Repository();
+        String choise;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("What product do you want to buy?");
+        choise = scan.nextLine().trim();
+    }
 
     // skriv in användarnan & lösenord
 

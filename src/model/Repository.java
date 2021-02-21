a1112a2b7ff23a3c3b2cd045cf97e906ff0f2da0
yadonx <ej_yo@hotmail.com>
2021-02-21 11:26:27 +0100

    updated some classes

--- src/model/Repository.java
+++ src/model/Repository.java
@@ -1,5 +1,11 @@
 package model;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  * Created by Emil Johansson
  * Date: 2021-02-21
@@ -8,5 +14,23 @@ package model;
  * Package: model
  */
 public class Repository {
+    private Properties properties = new Properties();
+
+
+    public Repository(){
+        try{
+            properties.load(new FileInputStream("src/Setting.Properties"));
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private Connection addConnection() throws SQLException {
+        return  DriverManager.getConnection(properties.getProperty("connectionString"),
+                properties.getProperty("name"),
+                properties.getProperty("password"));
+    }
+
 
 }

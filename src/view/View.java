9e550a79c6dbd20b70737c0a587d23d4203f54e6
yadonx <ej_yo@hotmail.com>
2021-02-22 14:16:27 +0100

    added some methods

--- src/view/View.java
+++ src/view/View.java
@@ -1,11 +1,13 @@
 package view;
 
-import model.Rating;
-import model.Repository;
+import controller.Controller;
+import model.Shoe;
 
 import java.sql.*;
+import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Emil Johansson
@@ -15,40 +17,52 @@ import java.util.Scanner;
  * Package: view
  */
 public class View {
+    public static void main(String[] args) throws SQLException {
+        new View();
+    }
     
-    Repository repo = new Repository();
+    Controller controller;
+    Scanner scan;
 
     // låta användaren lägga in produkter. ska ej kunna se databasens IDn
     public View() throws SQLException {
-        Repository repo = new Repository();
-        String choise;
+        controller = new Controller();
+        scan = new Scanner(System.in);
+
+
+//        String choise;
         int alternative;
-        Scanner scan = new Scanner(System.in);
-        alternative = scan.nextInt();
-
+//
         System.out.println("Välkommen till SkoShoppen");
-        System.out.println("Vill du logga in? \n1. Ja \n2. Nej");
-        switch (alternative){
-            case 1 -> repo.checkPassword("name","password");
-            case 2 -> System.out.println("Du måste logga in för att kunna göra en beställning");
+//        login();
+        System.out.println("Välj en produkt du vill lägga till i varukorgen.");
+        printAllShoes();
+        while (true) {
+            alternative = scan.nextInt();
+            if(addToCart(alternative))
+                break;
         }
 
-        System.out.println("Välj ett alternativ");
-        System.out.println("1. Gör en beställning");
-        System.out.println("2. Lägg till en vara i en befintlig beställning");
-        System.out.println("3. Kolla betyg på en produkt");
-        System.out.println("4. Ge betyg på en produkt");
-        System.out.println("5. Kolla vilka produkter som finns i lager");
-        System.out.println("6. Kolla vilka produkter som ligger i varukorgen");
 
-        switch (alternative) {
-            case 1 -> repo.getOrder();
-            case 2 -> repo.addToCart( 1); // ändra denna
-            case 3 -> Rating.class.toString();
-            case 4 -> 
-            case 5 -> stockBalance();
-            case 6 -> printAllProducts();
-        }
+//
+//
+//        System.out.println("Välj ett alternativ");
+//        System.out.println("1. Gör en beställning");
+//        System.out.println("2. Lägg till en vara i en befintlig beställning");
+//        System.out.println("3. Kolla betyg på en produkt");
+//        System.out.println("4. Ge betyg på en produkt");
+//        System.out.println("5. Kolla vilka produkter som finns i lager");
+//        System.out.println("6. Kolla vilka produkter som ligger i varukorgen");
+//        alternative = scan.nextInt();
+//
+//        switch (alternative) {
+//            case 1 -> repo.getOrder();
+//            case 2 -> repo.addToCart( 1); // ändra denna
+//            case 3 -> Rating.class.toString();
+//            case 4 ->
+//            case 5 -> stockBalance();
+//            case 6 -> printAllProducts();
+//        }
 
       /*  while(true) {
             System.out.print("Vilken sko vill du lägga till? (Skriv Q för att avsluta)");
@@ -61,8 +75,26 @@ public class View {
     }
 
     // skriv in användarnamn & lösenord
+    private void login(){
+        while (true) {
+            System.out.print("Användarnamn: ");
+            String userName = scan.nextLine();
+            System.out.print("Lösenord: ");
+            String password = scan.nextLine();
+            if (controller.login(userName, password)) {
+                System.out.println("Du är nu inloggad");
+                break;
+            } else System.out.println("Användarnamn och lösenord matchar inte! Försök igen");
+        }
+    }
 
     // skriv ut alla produkter i lager och välj produkt
+    public void printAllShoes(){
+        AtomicInteger counter = new AtomicInteger(1);
+        controller.getAllShoes().forEach(e -> System.out.println(counter.getAndIncrement() +": "+ e));
+    }
+
+
     public int stockBalance() throws SQLException {
         Properties properties = new Properties();
         try(Connection connection = DriverManager.getConnection(properties.getProperty("connectionString"),
@@ -82,12 +114,22 @@ public class View {
     }
 
     // stored procedures, produkten läggs till i beställningen
+    public Boolean addToCart(int shoe){
+        int listIndex = shoe - 1;
+        List<Shoe> shoes = controller.getAllShoes();
+        if (shoes.size()-1 < listIndex) {
+            System.out.println("Produkten finns inte\nFörsök igen.");
+            return false;
+        }
+        System.out.println(controller.addToCart(shoes.get(listIndex).getId()));
+        return true;
+    }
 
     // återkoppling om fel uppstod eller om det gått bra att lägga till varan (görs i Repository?)
 
     // möjlighet att skriva ut alla produkter som lagts i varukorgen
     public void printAllProducts(){
-        System.out.println(repo.getOrder().toString());
+        //System.out.println(repo.getOrder().toString());
     }
 
     // betyg och kommentarer ska kunna GES på produkter (VG)
